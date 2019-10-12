799bec449d107ac3358b5bd6943f70deaa4fe492
bolzano <ivanov.ivan.v@gmail.com>
2019-10-12 14:29:25 +0300

    add validator package, add mongoose schema User and Task

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',
     {
@@ -9,16 +10,55 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid');
+            }
+        }
     },
     age:{
-        type: Number
-    }
+        type: Number,
+        default: 0,
+        validate(value){
+            if(value < 0){
+                throw new Error('Age must be a positive number');
+            }
+        }
+    },
+    password:{
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 7,
+        validate(value){
+            if(value.includes("password")){
+                throw new Error("Password cannot contain the password word");
+            }
+        },
+    },
 });
 
-const me = new User({name: 'Ivan', age: 35});
+const me = new User({name: 'Ivan', age: 35,email: "ivanov.ivan.v@gmail.com", password: "password" });
 me.save().then(() => {
     console.log(me);
 }).catch(err => {
     console.log({err});
+});
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+    }
 });
