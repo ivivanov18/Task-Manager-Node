f49772393a15c8826aeb3dda0c5e448a4878d1cb
bolzano <ivanov.ivan.v@gmail.com>
2019-10-12 16:04:31 +0300

    - refactor task and user models into separate file - add routes to handle get users and tasks and get user by id - add routes to handle post user and task

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',
     {
@@ -8,57 +7,4 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',
     }
 );
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    email:{
-        type: String,
-        required: true,
-        trim: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Email is invalid');
-            }
-        }
-    },
-    age:{
-        type: Number,
-        default: 0,
-        validate(value){
-            if(value < 0){
-                throw new Error('Age must be a positive number');
-            }
-        }
-    },
-    password:{
-        type: String,
-        required: true,
-        trim: true,
-        minLength: 7,
-        validate(value){
-            if(value.includes("password")){
-                throw new Error("Password cannot contain the password word");
-            }
-        },
-    },
-});
 
-const me = new User({name: 'Ivan', age: 35,email: "ivanov.ivan.v@gmail.com", password: "password" });
-me.save().then(() => {
-    console.log(me);
-}).catch(err => {
-    console.log({err});
-});
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
-    },
-    completed: {
-        type: Boolean,
-        default: false,
-    }
-});
