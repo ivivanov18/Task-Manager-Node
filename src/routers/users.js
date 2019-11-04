7953974b895fdb5e9280310bdc25f6a185c669f7
bolzano <ivanov.ivan.v@gmail.com>
2019-11-04 21:44:13 +0200

    add possibility to fetch avatar picture of a given user by id

--- src/routers/users.js
+++ src/routers/users.js
@@ -127,4 +127,17 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
     }
 });
 
+router.get('/users/:id/avatar', async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id);
+        if(!user || !user.avatar){
+            throw new Error();
+        }
+        res.set('Content-Type', 'image/jpg');
+        return res.send(user.avatar);
+    }catch(e){
+    }
+
+});
+
 module.exports = router;
